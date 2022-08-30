9c92a9406d698513822d9abea7cd2ad6e5b33f2b
Nicolas264859 <emailnumber264859@gmail.com>
2022-08-30 15:30:06 +0200

    Add Username validators

--- players/models.py
+++ players/models.py
@@ -1,14 +1,17 @@
 from django.conf import settings
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.core.validators import MaxValueValidator, MinValueValidator, MinLengthValidator
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
+
 User._meta.get_field("email")._unique = True
 User._meta.get_field("email").blank = False
 User._meta.get_field("email").null = False
+User._meta.get_field("username").validators = [ASCIIUsernameValidator(), MinLengthValidator(6)]
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
